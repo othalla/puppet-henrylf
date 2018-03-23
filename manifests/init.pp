a687d3d6c4d28a3bccbde4c7dca56e82e177dbe7
othalla <othalla.lf@gmail.com>
2018-03-23 21:04:42 +0100

    [Add] first init to add user and clone dotfiles

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,15 +10,45 @@
 #   Explanation of what this parameter affects and what it defaults to.
 #   e.g. "Specify one or more upstream ntp servers as an array."
 #
-class henrylf (
-  $package_name = $::henrylf::params::package_name,
-  $service_name = $::henrylf::params::service_name,
-) inherits ::henrylf::params {
+class henrylf {
 
-  # validate parameters here
+  $user = 'henrylf'
 
-  class { '::henrylf::install': } ->
-  class { '::henrylf::config': } ~>
-  class { '::henrylf::service': } ->
-  Class['::henrylf']
+  $homedir = $::osfamily ? {
+    'FreeBSD' => "/usr/home/${user}",
+    default   => "/home/${user}"
+  }
+  $shell   = $::osfamily ? {
+    'FreeBSD' => "/usr/local/bin/bash",
+    default   => "/bin/bash"
+  }
+
+  user { $user:
+    ensure     => present,
+    uid        => 1001,
+    gid        => 1001,
+    groups     => $user,
+    home       => $home,
+    shell      => $shell,
+    managehome => true,
+    password   => '*',
+    comment    => 'Henrylefourbe',
+  }
+
+  vcsrepo { "${homedir}/othaDotfiles":
+    ensure   => latest,
+    provider => git,
+    remote   => 'origin',
+    revision => 'master',
+    user     => $user,
+    source   => 'https://github.com/othalla/othaDotfiles.git',
+  }
+  ~> exec { 'Install dotfiles':
+    environment => ["HOME=${homedir}"],
+    command     => "${home}/othaDotfiles/setup.sh",
+    logoutput   => true,
+    user        => $user,
+    refreshonly => true,
+  }
+
 }
